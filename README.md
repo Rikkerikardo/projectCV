adbc4a0aec822032bd0db7f02c1bc8bd73fb1961
Riku Pelkonen <pelkonen.riku@gmail.com>
2021-01-19 12:28:37 +0200

    defining structure

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Single-page application for personal CV
 ## Technologies
 
 React 17.0.1
+Material-UI 4.11.2
--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Card, Container, CardContent, Typography } from "@material-ui/core"
+import React from "react"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Container maxWidth="lg">
+        <Card>       
+          <CardContent>
+          <Typography>Basic info</Typography>
+        </CardContent>
+        <CardContent>
+          <Typography>Skills</Typography>
+        </CardContent>
+        <CardContent>
+          <Typography>Work experience</Typography>
+        </CardContent>
+        <CardContent>
+          <Typography>Education</Typography>
+        </CardContent>
+        <CardContent>
+          <Typography>About me</Typography>
+        </CardContent>
+        </Card>
+      </Container>
+  )
 }
 
-export default App;
+export default App
