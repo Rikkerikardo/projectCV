5d5c780a6286eb956b7c702c15b2d119fadcb18d
Riku Pelkonen <pelkonen.riku@gmail.com>
2021-01-25 18:28:55 +0200

    skills from accordion to list

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,65 +1,94 @@
-import { Accordion, Card, CardContent, Typography, AccordionSummary, AccordionDetails, Link } from "@material-ui/core"
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import { List, ListItem, ListItemText, Card, CardContent, Typography, Link, Divider, Collapse } from "@material-ui/core"
+import React, {useState} from "react"
+
 const Skills = () => {
+  const [first, setFirst] = useState(false)
+  const [second, setSecond] = useState(false)
+  const [third, setThird] = useState(false)
+
+  const handleClick = (index) => {
+    switch (index) {
+      case 0:
+        setFirst(!first)
+        break
+        case 1:
+          setSecond(!second)
+        break
+        case 2:
+        setThird(!third)
+        break   
+      default:
+        break
+    }
+  }
+
 
     return (
         <Card>       
         <CardContent>
         <Typography variant="h5">Skills</Typography>  
         <Typography variant="h6" align="center">Programming</Typography>
-        <Accordion> 
-          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography>Javascript</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
+        <List> 
+        <ListItem button 
+        onClick={() => handleClick(0)}>
+        <ListItemText primary="Javascript" align="center" />
+        </ListItem>
+        <Collapse in={first} timeout="auto" unmountOnExit>
+        <List>
+          <ListItem>
+            <Typography>
             <Link href="https://fullstackopen.com/en/">
-              Fullstack / University of Helsinki (8 course credit, 5/5 score)
+              - Fullstack / University of Helsinki (8 course credit, 5/5 score)
             </Link>
             <br />
             <Link href ="https://github.com/Rikkerikardo/fullstack-websovelluskehitys2020">
-              (Github repository of course)
+              - (Github repository of course)
             </Link>
           </Typography>
-        </AccordionDetails>
-        </Accordion>
-        <Accordion> 
-          <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-        >
-          <Typography>Java</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
+          </ListItem>
+        </List>
+      </Collapse>
+        <Divider />
+        <ListItem button
+        onClick={() => handleClick(1)}>
+          <ListItemText primary="Java" align="center" />
+        </ListItem>
+        <Collapse in={second} timeout="auto" unmountOnExit>
+        <List>
+          <ListItem>
           <Typography>
           <Link href="https://sisu.jyu.fi/student/courseunit/otm-5398e687-3268-48a3-b0a3-2d646f1a2b37/brochure">
-              Ohjelmointi 2 / University of Jyväskylä (8 course credit, 5/5 score)
+              - Ohjelmointi 2 / University of Jyväskylä (8 course credit, 5/5 score)
             </Link>
             <br />
             <Link href ="https://gitlab.jyu.fi/pelkrpxt/ohj2">
-              (Gitlab repository of course)
-            </Link>
-          
-          </Typography>
-        </AccordionDetails>
-        </Accordion>
-        <Accordion> 
-          <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-        >
-          <Typography>C#</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
+              - (Gitlab repository of course)
+            </Link>         
+          </Typography>       
+          </ListItem>
+        </List>
+      </Collapse>
+        <Divider />
+        <ListItem button 
+        onClick={() => handleClick(2)}>
+        <ListItemText primary="C#" align="center"  />
+        </ListItem>
+        <Collapse in={third} timeout="auto" unmountOnExit>
+        <List>
+          <ListItem>
           <Typography>
           <Link href="https://sisu.jyu.fi/student/courseunit/jy-CU-9321-v2/brochure">
-              Ohjelmointi 1 / University of Jyväskylä (6 course credit, 5/5 score)
+              - Ohjelmointi 1 / University of Jyväskylä (6 course credit, 5/5 score)
             </Link>
           </Typography>
-        </AccordionDetails>
-        </Accordion>
+          </ListItem>
+        </List>
+      </Collapse>
+        </List>
 
         <Typography variant="h6" align="center">Language</Typography>
-        <Typography align="left">Finnish - Native</Typography>
-        <Typography align="left">English - Proficient (C1/CEFR)</Typography>
+        <Typography align="center">Finnish - Native</Typography>
+        <Typography align="center">English - Proficient (C1/CEFR)</Typography>
        
       </CardContent>
       </Card>
